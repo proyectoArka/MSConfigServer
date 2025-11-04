126645f97c7386b8790bbdb16ff06613e81bf4e2
Esteban Villa <villa310esteban@gmail.com>
2025-11-03 21:03:36 -0500

    microservicio configServer

---
+++ src/main/java/com/arka/MSConfigServer/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.arka.MSConfigServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/actuator/**"))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
